27e1468175af5ecbb79b10cadc133e83bcff4f05
René Devichi <rene.devichi@gmail.com>
2019-01-01 08:04:30 +0100

    Python bumped to 3.7.2

--- python3.7.sh
+++ python3.7.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-echo "----------------------- Python 3.7 -----------------------"
+version=3.7.2
 
-wget -O /tmp/Python-3.7.1.tar.xz -nv -c https://www.python.org/ftp/python/3.7.1/Python-3.7.1.tar.xz
+echo "----------------------- Python ${version} -----------------------"
+
+wget -O /tmp/Python-${version}.tar.xz -nv -c https://www.python.org/ftp/python/${version}/Python-${version}.tar.xz
 
 sudo apt install -y \
     libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev \
     libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev
 
-tar -C /tmp -xaf /tmp/Python-3.7.1.tar.xz
-pushd /tmp/Python-3.7.1
+tar -C /tmp -xaf /tmp/Python-${version}.tar.xz
+pushd /tmp/Python-${version}
 # ./configure --enable-optimizations
 ./configure
 make -j4
